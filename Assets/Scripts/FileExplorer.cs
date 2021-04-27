2b12dec38ae48c7d61c7ae56ba89ddc7bbe07d1f
acj2r <79653782+acj2r@users.noreply.github.com>
2021-04-26 20:34:11 -0500

    Second Button

--- Assets/Scripts/FileExplorer.cs
+++ Assets/Scripts/FileExplorer.cs
@@ -23,4 +23,21 @@ public class FileExplorer : MonoBehaviour
    {
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
+
+//    Nibraas Code
+//    public void ConnectToRobot() {
+
+//         string path_ip = Application.dataPath + @"/RobotCode/Nao_Config.txt";
+//         lineChanger(ip_address.text, path_ip, 2);
+
+//         string path = Application.dataPath + @"/RobotCode/testSocketAndRobot.py";
+//         path = """ + path + """;
+//         string strCmdText = "/K cd "Assets/RobotCode" && python " + path;
+
+//         ProcessStartInfo startInfo = new ProcessStartInfo();
+//         startInfo.FileName = "CMD.exe";
+//         startInfo.Arguments = strCmdText;
+//         startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+//         Process.Start(startInfo);
+//     }
 }
